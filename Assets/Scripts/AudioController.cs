a27f22c87ff23d3faa92bf7ccda32842f1fbc5d5
Ramon Melo <me@ramonmelo.com>
2019-11-13 19:21:56 -0300

    ADDED: Audio Controller

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour
+{
+    private AudioSource source;
+    public static AudioSource instance;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        instance = source;
+    }
+}
